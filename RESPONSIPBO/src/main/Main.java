22bc8cd72c7684f93754176446e506a2cbb9451a
nurevafusfadewi <123190027@student.upnyk.ac.id>
2021-06-30 11:57:08 +0700

    responsi

---
+++ RESPONSIPBO/src/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Asus
+ */
+import view.*;
+import java.awt.event.*;
+import javax.swing.*;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+public class Main {
+    public static void main(String[] args) {
+        Menu g = new Menu();
+    } 
+}
+
